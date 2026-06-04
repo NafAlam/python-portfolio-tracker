9eaa4ff95fefaca31125b13d7e1a71809b805a80
Nafizul Alam <alam.nafizul63@gmail.com>
2026-06-04 22:55:20 +0100

    refactor: print formatting

--- main.py
+++ main.py
@@ -12,10 +12,19 @@ with open('trades.csv') as csvfile:
         portfolio.add_trade(trade)
 
 positions = portfolio.calculate_position()
-print(positions)
+print("Positions")
+for symbol, quantity in positions.items():
+    print(f"{symbol}: {quantity} shares")
+
+print()
 
 avg_entry_price = portfolio.average_entry_price()
-print(avg_entry_price)
+print("Average Entry Prices")
+for symbol, price in avg_entry_price.items():
+    print(f"{symbol}: £{price:.2f}")
+
+print()
 
 total_pnl = portfolio.realized_pnl()
-print(total_pnl)
+print("Realized PnL")
+print(f"£{total_pnl:.2f}")
